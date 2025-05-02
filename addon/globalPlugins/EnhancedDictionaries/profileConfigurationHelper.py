28f1b4d000a1b807bc7bc386714c21eb86f79f50
danieldamacena-ma <danieldamacena.ma@gmail.com>
2025-05-02 08:01:05 -0300

    changes spaces to tabs

--- addon/globalPlugins/EnhancedDictionaries/profileConfigurationHelper.py
+++ addon/globalPlugins/EnhancedDictionaries/profileConfigurationHelper.py
@@ -6,27 +6,27 @@ key = "keepUpdatedCheckbox"
 
 
 def stringToBool(value):
-    if value == "True":
-        return True
-    return False
+	if value == "True":
+		return True
+	return False
 
 
 def getSavedCheckboxValueForProfile():
-    if module not in config.conf:
-        log.info("o módulo não foi encontrado na configuração")
-        return False
-    if key not in config.conf[module]:
-        log.info("a chave não foi encontrada no módulo.")
-        return False
-    return stringToBool(config.conf[module][key])
+	if module not in config.conf:
+		log.info("o módulo não foi encontrado na configuração")
+		return False
+	if key not in config.conf[module]:
+		log.info("a chave não foi encontrada no módulo.")
+		return False
+	return stringToBool(config.conf[module][key])
 
 
 def saveCheckboxValueForProfile(value):
-    if module not in config.conf:
-        config.conf[module] = {}
-    if key not in config.conf[module]:
-        config.conf[module][key] = value
-        config.conf.save()
-    if config.conf[module][key] != value:
-        config.conf[module][key] = value
-        config.conf.save()
+	if module not in config.conf:
+		config.conf[module] = {}
+	if key not in config.conf[module]:
+		config.conf[module][key] = value
+		config.conf.save()
+	if config.conf[module][key] != value:
+		config.conf[module][key] = value
+		config.conf.save()
